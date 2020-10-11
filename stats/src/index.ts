072fd00a1904d2a338eb54207a75bbb2aa2bebb2
extasy44 <seoheejun@gmail.com>
2020-10-11 23:25:57 +1100

    reusable design pattern

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi');
+import { HtmlReport } from './reportTargets/HtmlReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
+
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+
+summary.buildAndPrintReport(matchReader.matches);
