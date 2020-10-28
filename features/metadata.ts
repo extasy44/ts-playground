2b4534c4333dc3ab5f8fa0f976204b67957b4b99
extasy44 <seoheejun@gmail.com>
2020-10-29 00:04:27 +1100

    Express and TS integration using decorators

---
+++ features/metadata.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+
+class Plane {
+  color: string = 'red';
+
+  @markFunction('Hi There')
+  fly(): void {
+    console.log('vrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetaData(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
---
+++ server/src/controllers/LoginContoller.ts
@@ -0,0 +1,46 @@
+import { Response, Request, NextFunction } from 'express';
+import { get, controller, use, bodyValidator, post } from './decorators';
+
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('Request was made');
+  next();
+}
+
+@controller('/auth')
+class LoginController {
+  @get('/login')
+  @use(logger)
+  getLogin(req: Request, res: Response): void {
+    res.send(`
+    <form method="post">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+    if (email && password && email === 'seo@gmail.com' && password === '1') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('invalid email or password');
+    }
+  }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = null;
+    res.redirect('/');
+  }
+}
